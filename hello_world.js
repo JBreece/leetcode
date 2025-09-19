65aefa2f83af6caf17b7a8ab008c8460bcea71d7
JBreece <joshbreece2012@gmail.com>
2025-09-19 16:46:55 -0400

    initial commit (hello world)

---
+++ hello_world.js
@@ -0,0 +1,15 @@
+/**
+ * @return {Function}
+ * lc 2667
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        return "Hello World";
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
